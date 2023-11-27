4a46c124145596507cef3456e891f31b5492d32a
Maruf Khan <maruf1611khan@gmail.com>
2023-11-27 14:29:23 +0530

    added cart page

---
+++ src/pages/Cart.js
@@ -0,0 +1,162 @@
+import React from 'react'
+import CartTable from '../components/CartTable'
+import { useState } from 'react'
+
+const Cart = () => {
+
+  const [cartItems, setCartItems] = useState(
+    [
+      {
+        id: 1,
+        image: '',
+        title: 'Mango',
+        price: 5000,
+        quantity: 1,
+      },
+      {
+        id: 2,
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Jan_Vermeer_van_Delft_-_The_Glass_of_Wine_-_Google_Art_Project.jpg/1024px-Jan_Vermeer_van_Delft_-_The_Glass_of_Wine_-_Google_Art_Project.jpg',
+        title: 'Kiwi',
+        price: 4500,
+        quantity: 1,
+      },
+    ]);
+
+  const increaseQuantity = (itemId) => {
+    setCartItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === itemId ? { ...item, quantity: item.quantity + 1 } : item
+      )
+    );
+  };
+
+  const decreaseQuantity = (itemId) => {
+    setCartItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === itemId && item.quantity > 1
+          ? { ...item, quantity: item.quantity - 1 }
+          : item
+      )
+    );
+  };
+
+  const cartTotal = cartItems.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  );
+
+  const [formData, setFormData] = useState({
+    name: '',
+    phoneNumber: '',
+    email: '',
+    address: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log('Form submitted:', formData);
+  };
+
+  return (
+
+    <div className=''>
+      <div className='text-3xl m-12'>Place Order</div>
+
+      <div className='flex flex-row justify-center items-center'>
+        <div className="max-w-md mx-auto mt-8">
+          <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">
+                Name:
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="John Doe"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="phoneNumber" className="block text-gray-700 text-sm font-bold mb-2">
+                Phone Number:
+              </label>
+              <input
+                type="tel"
+                id="phoneNumber"
+                name="phoneNumber"
+                value={formData.phoneNumber}
+                onChange={handleChange}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="123-456-7890"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
+                Email:
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="john@example.com"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="address" className="block text-gray-700 text-sm font-bold mb-2">
+                Address:
+              </label>
+              <textarea
+                id="address"
+                name="address"
+                value={formData.address}
+                onChange={handleChange}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="123 Main St, City, Country"
+                required
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+
+        <div className='m-10'>
+
+          <CartTable cartItems={cartItems} increaseQuantity={increaseQuantity} decreaseQuantity={decreaseQuantity} />
+        </div>
+      </div>
+
+
+      <div className="flex justify-end text-3xl mr-40 mt-9">
+        Total: ₹{cartTotal.toFixed(2)}
+        <div className='ml-10 bg-black text-white p-2 rounded-md'>Checkout</div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Cart
